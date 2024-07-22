fb404ecc506f7ff6b341dfb58f64503bd53823d6
Felixdrawe <131857058+Felixdrawe@users.noreply.github.com>
2024-07-22 10:41:30 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish: ['Mulish', 'sans-serif'],
+        rokkitt: ['Rokkitt', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
